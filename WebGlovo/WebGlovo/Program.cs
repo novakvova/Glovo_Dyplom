88b7b4eaff1a30a11a2228fcb91d7230a3b94e96
novakvova <novakvova@gmail.com>
2026-05-06 19:22:03 +0300

    Add Swagger

--- WebGlovo/WebGlovo/Program.cs
+++ WebGlovo/WebGlovo/Program.cs
@@ -1,10 +1,61 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        // Ensure instances exist
+        document.Components ??= new OpenApiComponents();
+        document.Components.SecuritySchemes ??= new Dictionary<string, IOpenApiSecurityScheme>();
+
+        document.Components.SecuritySchemes["Bearer"] = new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Name = "Authorization",
+            Description = "JWT Authorization header using the Bearer scheme. Example: \"Bearer {token}\""
+        };
+
+        document.Security = [
+            new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecuritySchemeReference("Bearer"),
+                    []
+                }
+            }
+        ];
+
+        document.SetReferenceHostDocument();
+
+        document.Servers = [
+            new OpenApiServer
+            {
+                Url = builder.Configuration["ServerRunUrl"]
+            }
+        ];
+
+        return Task.CompletedTask;
+    });
+});
+
 var app = builder.Build();
+
+app.MapOpenApi();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    options.OAuthUsePkce();
+});
 
 // Configure the HTTP request pipeline.
 
