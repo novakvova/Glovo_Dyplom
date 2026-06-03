a1e30e49d15b39487a5b7b468ed112308e916a06
novakvova <novakvova@gmail.com>
2026-06-03 18:57:06 +0300

    Forgot password server is good

---
+++ WebGlovo/Core/Interfaces/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using Core.Models.Account;
+
+namespace Core.Interfaces;
+
+public interface IAccountService
+{
+    public Task<string> LoginByGoogle(string token);
+    public Task<bool> ForgotPasswordAsync(ForgotPasswordModel model);
+    public Task<bool> ValidateResetTokenAsync(ValidateResetTokenModel model);
+    public Task ResetPasswordAsync(ResetPasswordModel model);
+}
---
+++ WebGlovo/Core/Services/AccountService.cs
@@ -0,0 +1,126 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using Core.Interfaces;
+using Core.Models.Account;
+using Core.SMTP;
+using Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+
+namespace Core.Services;
+
+public class AccountService(IJwtTokenService tokenService,
+    UserManager<UserEntity> userManager,
+    IConfiguration configuration,
+    IImageService imageService,
+    ISmtpService smtpService
+    ) : IAccountService
+{
+
+    public async Task<string> LoginByGoogle(string token)
+    {
+        using var httpClient = new HttpClient();
+
+        httpClient.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", token);
+
+        //configuration
+        string userInfo = configuration["GoogleUserInfo"] ?? "https://www.googleapis.com/oauth2/v2/userinfo";
+        var response = await httpClient.GetAsync(userInfo);
+
+        if (!response.IsSuccessStatusCode)
+            return null;
+
+        var json = await response.Content.ReadAsStringAsync();
+
+        var googleUser = JsonSerializer.Deserialize<GoogleAccountModel>(json);
+
+        var existingUser = await userManager.FindByEmailAsync(googleUser!.Email);
+        if (existingUser != null)
+        {
+            var userLoginGoogle = await userManager.FindByLoginAsync("Google", googleUser.GogoleId);
+
+            if (userLoginGoogle == null)
+            {
+                await userManager.AddLoginAsync(existingUser, new UserLoginInfo("Google", googleUser.GogoleId, "Google"));
+            }
+            var jwtToken = await tokenService.CreateTokenAsync(existingUser);
+            return jwtToken;
+        }
+        else
+        {
+            var user = new UserEntity
+            {
+                Email = googleUser.Email,
+                UserName = googleUser.Email,
+                FirstName = googleUser.FirstName,
+                LastName = googleUser.LastName
+            };
+
+            if (!String.IsNullOrEmpty(googleUser.Picture))
+            {
+                user.Image = await imageService.SaveImageFromUrlAsync(googleUser.Picture);
+            }
+
+            var result = await userManager.CreateAsync(user);
+            if (result.Succeeded)
+            {
+
+                result = await userManager.AddLoginAsync(user, new UserLoginInfo(
+                    loginProvider: "Google",
+                    providerKey: googleUser.GogoleId,
+                    providerDisplayName: "Google"
+                ));
+
+                await userManager.AddToRoleAsync(user, "User");
+                var jwtToken = await tokenService.CreateTokenAsync(user);
+                return jwtToken;
+            }
+        }
+
+        return string.Empty;
+    }
+    public async Task<bool> ForgotPasswordAsync(ForgotPasswordModel model)
+    {
+        var user = await userManager.FindByEmailAsync(model.Email);
+
+        if (user == null)
+        {
+            return false;
+        }
+
+        string token = await userManager.GeneratePasswordResetTokenAsync(user);
+        var resetLink = $"{configuration["ClientUrl"]}/reset-password?token={Uri.EscapeDataString(token)}&email={Uri.EscapeDataString(model.Email)}";
+
+        var emailModel = new EmailMessage
+        {
+            To = model.Email,
+            Subject = "Password Reset",
+            Body = $"<p>Click the link below to reset your password:</p><a href='{resetLink}'>Reset Password</a>"
+        };
+
+        var result = await smtpService.SendEmailAsync(emailModel);
+
+        return result;
+    }
+
+    public async Task<bool> ValidateResetTokenAsync(ValidateResetTokenModel model)
+    {
+        var user = await userManager.FindByEmailAsync(model.Email);
+
+        return await userManager.VerifyUserTokenAsync(
+            user,
+            TokenOptions.DefaultProvider,
+            "ResetPassword",
+            model.Token);
+    }
+
+    public async Task ResetPasswordAsync(ResetPasswordModel model)
+    {
+        var user = await userManager.FindByEmailAsync(model.Email);
+
+        if (user != null)
+            await userManager.ResetPasswordAsync(user, model.Token, model.NewPassword);
+    }
+
+}
